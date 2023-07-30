f2502ea89fc31b5dea49e7a9abec82939bb7f826
Ian Drilon <ian.drilon@mashupgarage.com>
2023-07-30 22:36:20 +0800

    fixing linters

--- app/sign-up/components/UploadProfile.tsx
+++ app/sign-up/components/UploadProfile.tsx
@@ -10,7 +10,7 @@ export const UploadProfile: React.FC = () => {
   )
   const response = useGetRegistration((state) => ({ ...state }), shallow)
 
-  const onUploadChange = (e: ChangeEvent<HTMLInputElement>) => {
+  const onUploadChange = (e: ChangeEvent<HTMLInputElement>): void => {
     const file = e.currentTarget.files as FileList
 
     if (file.length > 0) {
@@ -47,20 +47,21 @@ export const UploadProfile: React.FC = () => {
         <Button variant='primary' fontSize='1rem' mt='4' width={200}>
           {state.preview ? 'set image' : 'Upload image'}
         </Button>
-        <Input
-          type='file'
-          height='100%'
-          width='100%'
-          position='absolute'
-          top='0'
-          left='0'
-          opacity='0'
-          aria-hidden='true'
-          accept='image/*'
-          cursor='pointer'
-          display={!!state.preview ? 'none' : 'block'}
-          onChange={(e) => onUploadChange(e)}
-        />
+        {!state.preview && (
+          <Input
+            type='file'
+            height='100%'
+            width='100%'
+            position='absolute'
+            top='0'
+            left='0'
+            opacity='0'
+            aria-hidden='true'
+            accept='image/*'
+            cursor='pointer'
+            onChange={(e) => onUploadChange(e)}
+          />
+        )}
       </Box>
       <Text>Or</Text>
       <Text cursor='pointer' onClick={handleSkip}>
