718a93fd5d0007c42940de3bc532848a4c52707e
Yusri <yusripawaki2@gmail.com>
2023-12-09 12:16:13 +0800

    feat: display admin cred

---
+++ services/apis/account/users.js
@@ -0,0 +1,17 @@
+// Function to get user data using its userId
+import { auth, db } from 'services/firebase'
+import { updatePassword, updateEmail } from 'firebase/auth'
+import { updateDoc, doc, getDoc, Timestamp } from 'firebase/firestore'
+
+export const getUserData = async () => {
+  const userId = auth?.currentUser?.uid
+  try {
+    const userDoc = doc(db, 'adminUsers', userId)
+    const userSnapshot = await getDoc(userDoc)
+    const user = userSnapshot.data()
+
+    return user
+  } catch (error) {
+    return { error: true, message: error.message, status: error.code }
+  }
+}
